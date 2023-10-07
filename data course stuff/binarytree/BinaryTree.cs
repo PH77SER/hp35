abc2140c172802e0fc000dec806f9d96350d4149
PH77SER <doublea654@gmail.com>
2023-10-07 13:59:11 +0200

    binary tree

--- data course stuff/binarytree/BinaryTree.cs
+++ data course stuff/binarytree/BinaryTree.cs
@@ -2,52 +2,28 @@
 
 class BinaryTree
 {
-    public class Node
-    {
-        public int value;
-        public int key;
-        public Node? left;
-        public Node? right;
-        public Node(int key, int value)
-        {
-            this.value = value;
-            this.key = key;
-            this.right = null;
-            this.left = 
-            null;
-        }
-        public void print()
-        {
-            if(left != null)
-                left.print();
-            Console.WriteLine(" key: " + key + "\tvalue: " + value);
-            if(right != null)
-                right.print();
-        }
-    }
     public Node root;
     public BinaryTree(int key, int item)
     {
         this.root = new Node(key,item);
     }
-    public void add(int key, int value) //logic might be wrong on where to go and build new nodes
+    public void add(int key, int value)
     {
-        Node next = root;
+        Node? next = root;
         while (next != null)
         {
             if (key == next.key)
                 {next.value = value;return;}
-            else if (key < next.key && next.left != null)
-                next = next.left;
-            else if (next.right != null)
-                next = next.right;
-        }
-        if (next != null) //might not work
-            if (key < next.key)
-                next.left = new Node (key,value);
+            else if (key < next.key)
+                if (next.left == null)
+                    next.left = new Node(key,value);
+                else next = next.left;
             else
-                next.right = new Node(key,value);
-        return;
+                if (next.right == null)
+                    next.right = new Node(key,value);
+                else next = next.right;
+        }
+        next = new Node(key,value);
     }
     public void remove(int key)//no point implementing this
     {
--- data course stuff/binarytree/Main.cs
+++ data course stuff/binarytree/Main.cs
@@ -1,15 +1,29 @@
+using System.Diagnostics;
+using System.Drawing;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         Random rand = new Random();
-        int treesize = 10;
-        BinaryTree tree = new BinaryTree(10, 10+1000);
-        tree.add(1,1);
-        // for (int i = 1 ; i <= treesize ; i++)
-        // {
-        //     tree.add(i, i+1000);
-        // }
-        tree.root.print();
+        Stopwatch stop = new Stopwatch();
+        stop.Reset();
+        int[] treesize = {10, 100, 250, 500, 1000, 2500, 5000, 10000,100000, 1000000};
+        BinaryTree tree = new BinaryTree(3, 10+1000);
+
+        foreach (int size in treesize)
+        {
+            stop.Start();
+            for (int i = 1 ; i <= size ; i++)
+            {
+                tree.add(rand.Next(size), i);
+            }
+            stop.Stop();
+            Console.WriteLine(size + "&" + String.Format("{0:0.00}",(float)stop.ElapsedTicks/10000) + "ms" + "\\" + "\\");
+            stop.Reset();
+            //tree.root.print();
+
+        }
+
     }
 }
